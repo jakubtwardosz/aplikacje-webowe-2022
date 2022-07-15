9ce159b6a5b2956eecd6281e57dcb7f59c33ed91
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-07-15 12:37:47 +0200

    Add gitignore and init class

--- lab-01/script.js
+++ lab-01/script.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+var App = /** @class */ (function () {
+    function App(message) {
+        this.greeting = message;
+        this.helloWorld();
+    }
+    App.prototype.helloWorld = function () {
+        console.log("Hello world!");
+    };
+    App.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return App;
+}());
+var app = new App("world");
--- lab-01/script.ts
+++ lab-01/script.ts
@@ -1,1 +1,19 @@
-console.log("Hello world");
+class App {
+    greeting: string;
+
+    constructor(message: string) {
+        this.greeting = message;
+
+        this.helloWorld();
+    }
+
+    helloWorld(){
+        console.log("Hello world!");
+    }
+
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let app = new App("world");
