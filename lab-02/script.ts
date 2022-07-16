1806cdd206a7b83df61b4d2574a55bc3427d6665
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-07-16 16:06:22 +0200

    save timeStamp and key to array

--- lab-02/script.ts
+++ lab-02/script.ts
@@ -8,12 +8,24 @@ class App {
     snare: HTMLAudioElement;
     tink: HTMLAudioElement;
     tom: HTMLAudioElement;
+    channel: any = [];
 
     constructor() {
-        document.addEventListener('keypress', this.play);
+        document.addEventListener('keypress', (event : KeyboardEvent) => {
+            let arr: any = [];
+            let key = event.key;
+            let time = event.timeStamp;
+            arr.push(key, time);
+            this.channels(arr);
+            this.play(key);           
+        });   
     }
 
-    play(event: KeyboardEvent) {
+    channels(arr){
+        console.log(arr);
+    }
+
+    play(key) {
         this.boom = document.querySelector('[data-sound="boom"]') as HTMLAudioElement;
         this.clap = document.querySelector('[data-sound="clap"]') as HTMLAudioElement;
         this.hihat = document.querySelector('[data-sound="hihat"]') as HTMLAudioElement;
@@ -24,32 +36,41 @@ class App {
         this.tink = document.querySelector('[data-sound="tink"]') as HTMLAudioElement;
         this.tom = document.querySelector('[data-sound="tom"]') as HTMLAudioElement;
 
-        switch (event.key) {
+        switch (key) {
             case 'q':
+                this.boom.currentTime = 0;
                 this.boom.play();
                 break;
             case 'w':
+                this.clap.currentTime = 0;
                 this.clap.play();
                 break;
             case 'e':
+                this.hihat.currentTime = 0;
                 this.hihat.play();
                 break;
             case 'r':
+                this.kick.currentTime = 0;
                 this.kick.play();
                 break;
             case 't':
+                this.openhat.currentTime = 0;
                 this.openhat.play();
                 break;
             case 'a':
+                this.ride.currentTime = 0;
                 this.ride.play();
                 break;
             case 's':
+                this.snare.currentTime = 0;
                 this.snare.play();
                 break;
             case 'd':
+                this.tink.currentTime = 0;
                 this.tink.play();
                 break;
             case 'f':
+                this.tom.currentTime = 0;
                 this.tom.play();
                 break;
         }
