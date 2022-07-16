2ea9b14b3fbe56b8320315d70cfa2f6bec4bab72
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-07-16 13:49:20 +0200

    keypress on selected keys

--- lab-02/script.ts
+++ lab-02/script.ts
@@ -1,11 +1,39 @@
 class App {
 
     constructor() {
-        document.addEventListener("keypress", this.onKeyPress);  
+        document.addEventListener('keypress', this.play);
     }
 
-    onKeyPress(){
-        console.log('Click');
+    play(event: KeyboardEvent) {
+        switch (event.key) {
+            case 'q':
+                console.log(event.key);
+                break;
+            case 'w':
+                console.log(event.key);
+                break;
+            case 'e':
+                console.log(event.key);
+                break;
+            case 'r':
+                console.log(event.key);
+                break;
+            case 't':
+                console.log(event.key);
+                break;
+            case 'a':
+                console.log(event.key);
+                break;
+            case 's':
+                console.log(event.key);
+                break;
+            case 'd':
+                console.log(event.key);
+                break;
+            case 'f':
+                console.log(event.key);
+                break;
+        }
     }
 }
 
