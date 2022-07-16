fffd4d816f109d4b71ca704a367f102ea239990b
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-07-16 13:35:39 +0200

    Add html elements onKeyPress event listener

--- lab-02/script.ts
+++ lab-02/script.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+class App {
+
+    constructor() {
+        document.addEventListener("keypress", this.onKeyPress);  
+    }
+
+    onKeyPress(){
+        console.log('Click');
+    }
+}
+
+let app = new App();
+
+
+
+
