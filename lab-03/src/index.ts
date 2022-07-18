b4342f917508618f213a8592e9b015106ef81837
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-07-18 22:30:58 +0200

    push cities to localstorage

--- lab-03/src/index.ts
+++ lab-03/src/index.ts
@@ -6,11 +6,17 @@ const weatherService = new WeatherService();
 const weatherView = new WeatherView();
 
 class Main {
+  cities: string[] = [];
+
   constructor() {
     let city = document.getElementById('city') as HTMLInputElement;
     let button = document.getElementById('form');    
     button.addEventListener('submit', (event) => {
       event.preventDefault();
+      if(this.cities.includes(city.value)) return;
+        this.cities.push(city.value);
+        console.log(this.cities);
+        localStorage.setItem('cities', JSON.stringify(this.cities));
       if(city.value){
         weatherView.displayWeather(weatherService.getWeather(city.value))
       }
