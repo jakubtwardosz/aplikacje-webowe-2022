031fe4de209440df7fe59ec1912751ac0999867f
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-07-18 17:55:25 +0200

    get city weather on click

--- lab-03/src/index.ts
+++ lab-03/src/index.ts
@@ -4,8 +4,18 @@ import { Weather } from './weather';
 const weather = new Weather();
 
 class Main {
-  constructor(){
-    console.log('index');
+  constructor() {
+    let city = document.getElementById('city') as HTMLInputElement;
+    let button = document.getElementById('button');    
+    button.addEventListener('click', () => {
+      if(city.value){
+        console.log(weather.getWeather(city.value));
+      }
+    });
+
+
+
+    
   }
 }
 
--- lab-03/src/weather.ts
+++ lab-03/src/weather.ts
@@ -3,11 +3,9 @@ export class Weather {
     owApiKey = '51069f88456216e76d29bc1c50d8e00f';
 
     constructor() {
-        console.log('weather');
-        console.log(this.getWeather('Kraków'));
     }
 
-    async getWeather(city: string): Promise<any> {
+    async getWeather(city: string): Promise<string> {
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.owApiKey}&units=metric`;
         const response = await fetch(url);
         const data = await response.json();        
