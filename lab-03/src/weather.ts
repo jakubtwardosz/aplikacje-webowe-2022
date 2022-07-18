34917af7b2abeb8878a64d6acc595e26d598a1b1
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-07-18 17:13:47 +0200

    first api call

--- lab-03/src/weather.ts
+++ lab-03/src/weather.ts
@@ -1,5 +1,18 @@
 export class Weather {
+
+    owApiKey = '51069f88456216e76d29bc1c50d8e00f';
+
     constructor() {
         console.log('weather');
+        console.log(this.getWeather('Kraków'));
     }
+
+    async getWeather(city: string): Promise<any> {
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.owApiKey}&units=metric`;
+        const response = await fetch(url);
+        const data = await response.json();        
+        return data;
+    }
+
+
 }
