aac23ce686d359ba757aab7bdd2b4166db3045fa
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2022-07-20 02:15:49 +0200

    display note from database

--- lab-04/src/firebaseService.ts
+++ lab-04/src/firebaseService.ts
@@ -2,7 +2,9 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 import { firebaseConfig } from "./firebaseConfig";
 import { Note } from './note';
-import { doc, setDoc, getDoc, collection, getDocs } from "firebase/firestore";
+import { doc, setDoc, getDoc, addDoc, collection, getDocs } from "firebase/firestore";
+
+
 
 export class FirebaseService {
 
@@ -12,9 +14,8 @@ export class FirebaseService {
 
     // https://firebase.google.com/docs/firestore/manage-data/add-data?hl=en&authuser=0
     async addNote(note: Note) {
-        // Add a new document in collection "cities"
-        await setDoc(doc(this.db, "notes", note.title), {
-            name: note.title,
+        await addDoc(collection(this.db, "notes"), {
+            title: note.title,
             content: note.content,
             color: note.color
         });
@@ -23,8 +24,9 @@ export class FirebaseService {
     async getNotes() {
         const querySnapshot = await getDocs(collection(this.db, "notes"));
         querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
+            let data = doc.data();
+            let note = new Note(data.title, data.content, data.color, doc.id);
+            note.create();
         });
     }
 
--- lab-04/src/note.ts
+++ lab-04/src/note.ts
@@ -1,16 +1,32 @@
 export class Note {
-
+    
     title : string;
     content : string;
     color : string;
+    id : string;
 
-    constructor(title : string, content : string, color : string) {
+    constructor(title : string, content : string, color : string, id?: string) {
         this.title = title;
         this.content = content;
         this.color = color;
+        this.id = id;
     }
 
+    create(){
+        let container = document.getElementById('container');
+        let div = document.createElement('div');
+        if (this.id) {
+            div.id = this.id;
+        }        
+        container.appendChild(div);
 
+        div.innerHTML = `
+            <h2>${this.title}</h2>
+            <p>${this.content}</p>
+            <p>${this.color}</p>
+        `;
+
+    }
 }
 
 
